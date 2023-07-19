587892c815ec1985e90ed77e20ae1d9e666e4388
manothivya <92164653+manothivya@users.noreply.github.com>
2023-07-19 08:14:07 +0530

    Add files via upload

---
+++ api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public string Get()
+    {
+        string ip= Response.HttpContext.Connection.RemoteIpAddress.ToString()??"no ip found"; 
+        return ip;
+    }
+}
+     
